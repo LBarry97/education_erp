fc705d34d88a7b98d931f8a0253305b696fca867
Lansana Barry Sow <lansanabarry97@gmail.com>
2019-10-23 18:46:41 +0200

    [IMP] Añadir el modulo 'ihsan/ihsan_base'

---
+++ src/extra_addons/ihsan/ihsan_base/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ihsan Base',
+    'version': '11.0.1.0.0',
+    'license': 'LGPL-3',
+    'category': 'Education',
+    "sequence": 1,
+    'summary': 'Manage Students, Faculties and Education Institute',
+    'complexity': "easy",
+    'author': 'Alhasan',
+    'website': 'http://www.ihsanmallorca.es',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'test': [
+
+    ],
+    'css': [],
+    'qweb': [
+
+    ],
+    'js': [],
+    'images': [
+    
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
